dcda012c342fc4054cfef24642f99c31322f42d5
dwarfcrank <dwarfcrank@gmail.com>
2010-11-09 02:59:19 +0000

    Made the console sample a bit better.

--- samples/hello/source/main.c
+++ samples/hello/source/main.c
@@ -15,9 +15,26 @@ int main(int argc, const char* argv[])
 	// con_printf works exactly as you'd expect from printf.
 	con_printf( "Hello World!\n" );
 	
-	sleep( 15 );
+	// You can also get the dimensions of the console
+	int w, h;
+	con_get_dimensions( &w, &h );
+	con_printf( "This console has %d columns and %d rows.\n", w, h );
+	
+	// Color changing is also supported. The RGB macro is provided
+	// for convenience (see console.h)
+	con_set_foreground_color( RGB( 0, 255, 0 ) );
+	con_printf( "This text is green.\n" );
+	
+	// You can manipulate the coordinates the text will be drawn to.
+	// The coordinates are specified in characters.
+	con_set_xy( 10, 10 );
+	con_printf( "This text is faaaaar away from the rest.\n" );
+	
+	// Wait 10 seconds so you can actually see something.
+	sleep( 10 );
 	
 	// It's a good habit to shutdown the console after you're done.
+	// It frees some memory used internally by the library.
 	con_shutdown();
 	
 	return 0;
