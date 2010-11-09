50a4b4be898467388eeb3ad3b110da20c08a0ccc
dwarfcrank <dwarfcrank@gmail.com>
2010-11-09 03:04:15 +0000

    Added missing info to font.h

--- psl1ght/lib/libconsole/source/font.h
+++ psl1ght/lib/libconsole/source/font.h
@@ -1,3 +1,7 @@
+// This font (CP437.F16) comes from ftp://ftp.simtel.net/pub/simtelnet/msdos/screen/fntcol16.zip
+// The package is (c) by Joseph Gil
+// The individual fonts are public domain.
+
 #pragma once
 
 #include <stdint.h>
